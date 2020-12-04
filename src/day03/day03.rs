a788ba925d819eb2c384cdb5e36ecfcddb8780de
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2020-12-04 22:33:23 +0000

    Day 3

---
+++ src/day03/day03.rs
@@ -0,0 +1,54 @@
+use aoc2020::Vector2;
+use aoc2020::{parse_grid, Grid};
+
+fn trees_in_slope(grid: &Grid, slope: Vector2) -> u64 {
+    let mut pos = Vector2::new(0, 0);
+
+    let mut tree_count = 0;
+    while pos.y < grid.rows {
+        pos += slope;
+        if pos.x >= grid.cols {
+            pos.x = pos.x % grid.cols;
+        }
+        if let Some(&c) = grid.data.get(&pos) {
+            if c == '#' {
+                tree_count += 1;
+            }
+        }
+    }
+    tree_count
+}
+
+pub fn part1(input: &str) -> u64 {
+    let grid = parse_grid(input);
+    trees_in_slope(&grid, Vector2::new(3, 1))
+}
+
+pub fn part2(input: &str) -> u64 {
+    let grid = parse_grid(input);
+    let slopes = [
+        Vector2::new(1, 1),
+        Vector2::new(3, 1),
+        Vector2::new(5, 1),
+        Vector2::new(7, 1),
+        Vector2::new(1, 2),
+    ];
+    slopes
+        .iter()
+        .map(|&slope| trees_in_slope(&grid, slope))
+        .product()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn part1() {
+        let input = include_str!("input");
+        assert_eq!(super::part1(input), 254);
+    }
+    #[test]
+    fn part2() {
+        let input = include_str!("input");
+        assert_eq!(super::part2(input), 1666768320);
+    }
+}
---
+++ src/day03/mod.rs
@@ -0,0 +1,4 @@
+mod day03;
+
+pub use day03::part1;
+pub use day03::part2;
