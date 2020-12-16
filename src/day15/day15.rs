81c0ebb80818c809e18953acd76fdae31b4815e0
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2020-12-16 23:27:16 +0000

    Fix what was definitely not a real english sentence

--- src/day15/day15.rs
+++ src/day15/day15.rs
@@ -12,7 +12,11 @@ fn memory_game(mut nums: Vec<u32>, nth: u32) -> u32 {
     let mut seen: HashMap<u32, u32> = HashMap::new();
 
     let turn = nums.len() as u32 + 1;
-    // we dont want the last number in the seen map yet as we add last turn after it
+    // we dont want the last number in the seen map yet as we only add the number
+    // from previous turn on the turn after it. This allows us to only keep a single
+    // number inside the HashMap instead of the last two indexes. Whenever we have to
+    // look up an index, we know last one is for sure on turn -1, and are only required
+    // to look up the other index.
     let mut last = nums.pop().unwrap();
     // starting numbers
     for (idx, &num) in nums.iter().enumerate() {
